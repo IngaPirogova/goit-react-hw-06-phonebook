1720037321139ddef6d0682d7adc8a393118f208
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-02-23 22:07:29 +0200

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,6 +17,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'root',
   storage,
+  blacklist: ['filter'],
 };
 
 const rootReducer = combineReducers({
@@ -34,4 +35,6 @@ export const store = configureStore({
       },
     }),
 });
+
+
 export const persistor = persistStore(store);
